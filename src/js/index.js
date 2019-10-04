9b64bf4678d8e57f4bf9fdafb991e9495f62c2b5
Jan <jjirsik@gmail.com>
2019-10-04 15:25:23 +0200

    milestone 3 done

--- src/js/index.js
+++ src/js/index.js
@@ -12,11 +12,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const entity1 = new Entity(1,2,'apple');
   entity1.mount(stage);
 
-  const entity2 = new Entity(2,4,'apple');
+  const entity2 = new Entity(2,4,'bomb');
   entity2.mount(stage);
 
   const entity3 = new Entity(3,2,'wall');
   entity3.mount(stage);
+
+  const entity4 = new Entity(2,0,'bomb');
+  entity4.mount(stage);
+
+  const entity5 = new Entity(2,2,'bomb');
+  entity5.mount(stage);
 
 
   document.addEventListener('keydown', (event) => {
--- src/js/pacman.js
+++ src/js/pacman.js
@@ -8,6 +8,7 @@ class Pacman {
     this.direction = 0;
     this.stage = stage;
     this.score = 0;
+    this.death = false;
   }
 
   render() {
@@ -25,58 +26,68 @@ class Pacman {
   }
 
   move(direction) {
-    this.lastXpos = this.xpos;
-    this.lastYpos = this.ypos;
-    switch (direction) {
-      case "right":
-        if (this.xpos < this.maxWidth) {
-          this.xpos += 1;
-        }
-        this.direction = 0;
-        break;
-      case "left":
-        if (this.xpos > 0) {
-          this.xpos -= 1;
-        }
-        this.direction = 255;
-        break;
-      case "up":
-        if (this.ypos > 0) {
-          this.ypos -= 1;
-        }
-        this.direction = 85;
-        break;
-      case "down":
-        if (this.ypos < this.maxHeight) {
-          this.ypos += 1;
-        }
-        this.direction = 170;
-        break;
-    }
-    const collisionDetectionResult = this.stage.collisionDetection(
-      this.xpos,
-      this.ypos
-    );
-    
-    if (collisionDetectionResult !== null) {
-      switch (collisionDetectionResult.type) {
-        case "wall":
-          this.xpos = this.lastXpos;
-          this.ypos = this.lastYpos;
+    if (!this.death) {
+      this.lastXpos = this.xpos;
+      this.lastYpos = this.ypos;
+      switch (direction) {
+        case "right":
+          if (this.xpos < this.maxWidth) {
+            this.xpos += 1;
+          }
+          this.direction = 0;
           break;
-        case "apple":
-          this.score++;
-          this.stage.removeEntity(collisionDetectionResult);
+        case "left":
+          if (this.xpos > 0) {
+            this.xpos -= 1;
+          }
+          this.direction = 255;
+          break;
+        case "up":
+          if (this.ypos > 0) {
+            this.ypos -= 1;
+          }
+          this.direction = 85;
+          break;
+        case "down":
+          if (this.ypos < this.maxHeight) {
+            this.ypos += 1;
+          }
+          this.direction = 170;
           break;
       }
-    }
+      const collisionDetectionResult = this.stage.collisionDetection(
+        this.xpos,
+        this.ypos
+      );
+      
+      if (collisionDetectionResult !== null) {
+        switch (collisionDetectionResult.type) {
+          case "wall":
+            this.xpos = this.lastXpos;
+            this.ypos = this.lastYpos;
+            break;
+          case "apple":
+            this.score++;
+            this.stage.removeEntity(collisionDetectionResult);
+            break;
+          case "bomb":
+            if (Math.random() < 0.5) {
+              this.stage.removeEntity(collisionDetectionResult);
+              this.element.className = "entity entity--pac entity--tomb";
+              this.death = true;
+            }
+            
+            break;
+        }
+      }
 
-    if (this.mouth === 0) {
-      this.mouth = 85;
-    } else {
-      this.mouth = 0;
+      if (this.mouth === 0) {
+        this.mouth = 85;
+      } else {
+        this.mouth = 0;
+      }
+      this.update();
     }
-    this.update();
   }
 
   update() {
