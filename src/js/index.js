c2bdef46f0fa4b91b79c7c1476314883d8b3f004
JT <jakubtalacko@gmail.com>
2019-10-04 09:54:11 +0200

    stage1 completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('it works');
+  const pacman = document.querySelector('.pacboy-active-light');
+
+  let pacMouth = 85;
+  document.addEventListener('click', () => {
+    if(pacMouth === 0){
+    pacMouth = 85;
+    }else{
+      pacMouth = 0;
+    }
+    pacman.style.backgroundPositionX = `${pacMouth}px`;
+  });
 });
+
+    // background-position-x: 85px;
